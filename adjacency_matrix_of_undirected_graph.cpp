bec0d2e7cbc6573ded032c26b089f31889208c9f
meherab nahin <nahinrahman87@gmail.com>
2025-09-21 20:27:32 +0600

    graph representation and adjacency matrix of undirected grapgh

---
+++ adjacency_matrix_of_undirected_graph.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int node,edge;
+  cin>>node>>edge;
+
+  int adj_matrix[node][node];
+
+//   for(int i=0;i<node;i++){
+//     for(int j=0;j<node;j++){
+//         adj_matrix[i][j]=0;
+//     }
+//   }
+
+  
+memset(adj_matrix,0,sizeof(adj_matrix));//shortcut for making all value zero of adjacency matrix
+
+
+ 
+for(int i=0;i<node;i++)
+    for(int j=0;j<node;j++)
+        if(i==j)adj_matrix[i][j]=1;
+
+
+  while(edge--){
+    int a,b;
+    cin>>a>>b;
+    adj_matrix[a][b]=1;
+    adj_matrix[b][a]=1;
+
+  }
+
+  
+   for(int i=0;i<node;i++){
+    for(int j=0;j<node;j++){
+        cout<<adj_matrix[i][j]<<" ";
+        
+    }
+    cout<<endl;
+  }        
+
+
+   
+   
+  return 0;
+}
