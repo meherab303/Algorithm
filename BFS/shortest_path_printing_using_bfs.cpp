ed23b9398222e9b99e1ce6b30e80d98456a7ce83
meherab nahin <nahinrahman87@gmail.com>
2025-10-01 18:33:43 +0600

    shortest path printing

---
+++ BFS/shortest_path_printing_using_bfs.cpp
@@ -0,0 +1,78 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int>adj_list[1005];
+bool visited[1005];
+int level[1005];
+int parent[1005];
+
+void bfs(int src){
+    
+    queue<int>q;
+    q.push(src);
+    visited[src]=true;
+    level[src]=0;
+    cout<<"bfs travers --> ";
+    while(!q.empty()){
+        int par=q.front();
+        q.pop();
+        
+       
+        cout<<par<<" ";
+
+        for(int child:adj_list[par]){
+            if(!visited[child]){
+                q.push(child);
+                visited[child]=true;
+                level[child]=level[par]+1;
+                parent[child]=par;  //tracking every childs parent;
+            }
+        }
+    }
+    
+}
+
+int main()
+{
+  
+  int vertex,edge;
+  cin>>vertex>>edge;
+  
+  while(edge--){
+    int a,b;
+    cin>>a>>b;
+    adj_list[a].push_back(b);
+    adj_list[b].push_back(a);
+
+  }
+
+  memset(visited,false,sizeof(visited));
+  memset(level,-1,sizeof(level));
+  memset(parent,-1,sizeof(parent)); // intially making all vertexs parent -1;
+
+  int src,desti;
+  cin>>src>>desti;
+
+  bfs(src);
+  cout<<endl;
+
+  for(int i=0;i<vertex;i++){
+    cout<<i<<" parent-->"<<parent[i]<<endl;
+  }
+
+  vector<int>path;  //taking vector for reverse the path as we start from the destination here
+  int desti_vertex=desti;
+
+  while(desti_vertex!=-1){
+    path.push_back(desti_vertex);
+    desti_vertex=parent[desti_vertex];
+  }
+  
+  reverse(path.begin(),path.end()); //reverse the path
+
+  cout<<"shortest route from source "<<src<<" to destination "<<desti<<" is ";
+  for(int x:path){
+    cout<<x<<" ";
+  }
+  return 0;
+}
